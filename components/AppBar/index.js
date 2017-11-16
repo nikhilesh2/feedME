5d92b0bdea6db6dda15c8e58f2c7d84beab0e577
Singh <singh@Singhs-MBP.fios-router.home>
2017-11-16 16:47:24 -0500

    done with first iteration of calendar strip

--- components/AppBar/index.js
+++ components/AppBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
+import { View, Text, StyleSheet, Platform, ScrollView } from 'react-native';
 import { Toolbar } from 'react-native-material-ui';
 import { UITheme } from '../../utils/MuiTheme';
 import { height, width } from '../../utils/getDimensions';
@@ -22,29 +22,88 @@ export default class AppBar extends React.Component {
          </View>
         </View>
         <View style={innerWrapper}>
-    	   <Text style={{ color: 'white', fontSize: 24, flex: 1 }}>University of Pittsburgh</Text>
-         <Text style={{ color: 'white', fontSize: 56, flex: 2 }}>14</Text>
-         <Text style={{ color: 'white', fontSize: 18, flex: 3, opacity: 0.9 }}>Near you</Text>
+    	   <Text style={{ color: 'white', fontSize: 24, flex: 1.25 }}>University of Pittsburgh</Text>
+         <Text style={{ color: 'white', fontSize: 56, flex: 2.25 }}>14</Text>
+        {/*} <Text style={{ color: 'white', fontSize: 18, flex: 1, opacity: 0.9 }}>Near you</Text>*/}
         </View>
-
-        <View>
-               
+        <ScrollView contentContainerStyle={calendarStrip.contentContainer}>
+           <Text style={{ color: 'white', fontSize: 18, paddingBottom: 10 }}>November</Text>
+           <View style={{ flexDirection: 'row', alignSelf: 'center', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+             <Text style={dowStyle}>Sun</Text>
+             <Text style={dowStyle}>Mon</Text>
+             <Text style={dowStyle}>Tue</Text>
+             <Text style={dowStyle}>Wed</Text>
+             <Text style={dowStyle}>Thur</Text>
+             <Text style={dowStyle}>Fri</Text>
+             <Text style={dowStyle}>Sat</Text>
+           </View>
+           <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+             <Text style={dayStyle}>12</Text>
+             <Text style={dayStyle}>13</Text>
+             <Text style={dayStyle}>14</Text>
+             <Text style={dayStyle}>15</Text>
+             <View style={dayActiveOuter}>
+              <View style={dayActiveInner}>
+                <Text style={{ color: UITheme.palette.primaryColor, fontSize: 20, textAlign: 'center', top: 3}}>16</Text>
+              </View>
             </View>
-              
+             <Text style={dayStyle}>17</Text>
+             <Text style={dayStyle}>18</Text>
+             
+           </View>
+          
+        </ScrollView>
+        
       </View>
        
     );
   }
 }
+const dayActiveOuter = {
+  borderRadius: 200,
+  alignItems:'center', 
+  flex: 1.25,
+}
+const dayActiveInner = {
+  backgroundColor: 'white',
+  overflow: 'hidden',
+  borderRadius: 200,
+  alignSelf:'center', 
+  width: 30,
+  height: 30,
 
+}
+const dayStyle = {
+  color: 'white', 
+  fontSize: 20,
+  opacity: 1,
+  flex: 1.25,
+  textAlign: 'center',
+}
 
+const dowStyle = {
+  color: 'white', 
+  fontSize: 12,
+  opacity: 0.9,
+  flex: 1.25,
+  textAlign: 'center',
+}
 
 const style = {
   height: (Platform.OS === 'ios') ? 100 : 0, //this is just to test if the platform is iOS to give it a height of 20, else, no height (Android apps have their own status bar)
   backgroundColor: UITheme.palette.primaryColor,
-  height: height/2.5,
+  height: height/2.35,
   paddingTop: (Platform.OS === 'ios') ? 30 : 0, //this is just to test if the platform is iOS to give it a height of 20, else, no height (Android apps have their own status bar)
 }
+const calendarStrip = StyleSheet.create({
+  contentContainer: {
+    flex: 0.7,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 35
+  }
+});
 
 const innerWrapper = {
   flex: 1,
@@ -56,13 +115,6 @@ const innerWrapper = {
 const displayOption = {
   height: 50,
   flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
- 
-}
-const spacer = {
-  flex: 0.2,
-  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
  
